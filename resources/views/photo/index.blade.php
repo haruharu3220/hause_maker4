e1a4b626c8700950b892e4e18b8ee34476105710
hayaharu3220 <hayato.314.nico@gmail.com>
2023-04-01 09:47:20 +0000

    サイドバーの実装

--- resources/views/photo/index.blade.php
+++ resources/views/photo/index.blade.php
@@ -26,6 +26,7 @@
     <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css">-->
     <link href="{{ asset('css/lightbox.css') }}" rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('css/test.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/side.css') }}">
 </head>
   
   <x-app-layout>
@@ -37,119 +38,130 @@
     </x-slot>
     
 
-<form action="{{ route('photo.index') }}" method="GET">
-        <h2>検索</h2>
-    <p>開始日：</p>
-    <input type="date" name="start">
+    <form action="{{ route('photo.index') }}" method="GET">
+            <h2>検索</h2>
+        <p>開始日：</p>
+        <input type="date" name="start">
+        
+        <p>~終了日：</p>
+        <input type="date" name="end">
     
-    <p>~終了日：</p>
-    <input type="date" name="end">
-
-    <button type="submit">送信</button>
-</form>
+        <button type="submit">送信</button>
+    </form>
+    
+    <!--★★★★★★★★★★★★★★★★★★★★★★★★★★ここから★★★★★★★★★★★★★★★★★★★★★★★-->
+    <div class="contents">
+      <div id="sidebar">
+        <nav id="global-nav">
+          <ul class="sort-btn">
+           <li class="sub-menu">
+              <a href="#"><dt>All</dt></a>
+                <ul class="sub-menu-nav">  <!--unordered list 順不同リスト-->
+                  <li class="all active">全て</li>  <!--list item リスト項目-->
+                </ul>
+            </li>
+ 
+            <li class="sub-menu">
+              <a href="#"><dt>部屋</dt></a>
+                <ul class="sub-menu-nav">
+                  @foreach($tags as $tag)
+                    <li class="tag_no_{{$tag->id}}"><a href="#">{{$tag->name}}</a></li>
+                  @endforeach
+                </ul>
+            </li>
+        
+            <li class="sub-menu">
+              <a href="#"><dt>タイプ</dt></a>
+                <ul class="sub-menu-nav">
+                  @foreach($types as $type)
+                    <li class="type_no_{{$type->id}}"><a href="#">{{$type->name}}</a></li>
+                  @endforeach
+                </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    <!--★★★★★★★★★★★★★★★★★★★★★★★★★★ここまで★★★★★★★★★★★★★★★★★★★★★★★-->
     
     
-    <ul class="sort-btn"> 
-      <li>
-        <dl>  <!--description list 説明リスト-->
-          <dt>All</dt>  <!--description list term 項目名や用語-->
-          <dd>  <!--description description 説明や定義、値-->
-            <ul>  <!--unordered list 順不同リスト-->
-              <li class="all active">全て</li>  <!--list item リスト項目-->
-            </ul>
-          </dd>
-        </dl>
-      </li>
-      <li>
-        <dl>
-          <dt>タイプ</dt>
-          <dd>
-            <ul>
-              @foreach($types as $type)
-                <li class="type_no_{{$type->id}}">{{$type->name}}</li>
+      <div class="main_content">
+        <!--ここから-->
+ 
+        <!--ここまで-->
+      
+        <ul class="grid">
+        @foreach ($photos as $photo)
+          <li class="item {{$photo->tag_no}} type_no_{{$photo->type_id}} cat03">
+            <!--内側のdivには高さを維持するためにitem-contentというクラス名をつける。-->
+            <div class="item-content">
+              <div>投稿日：{{ date('Y年m月d日', strtotime($photo->created_at)) }}</div>
+              <a href="{{ asset('storage/image/'.$photo->image)}}" data-lightbox="picture" data-title="{{$photo->name}}">
+                  <img src="{{ asset('storage/image/'.$photo->image)}}" class="modal-trigger mx-auto" >
+              </a>
+              
+              <span class="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-md text-xs font-medium bg-red-100 text-blue-800">{{$photo->type_name}}</span>
+              
+              @foreach ($photo->tag_names as $tag_name)
+              <span class="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-md text-xs font-medium bg-blue-100 text-blue-800">{{$tag_name}}</span>
               @endforeach
-            </ul>
-          </dd>
-        </dl>
-      </li>
-      <li>
-        <dl>
-        <dl>
-          <dt>設備・部屋</dt>
-          <dd>
-            <ul>
-              @foreach($tags as $tag)
-                <li class="tag_no_{{$tag->id}}">{{$tag->name}}</li>
-              @endforeach
-            </ul>
-          </dd>
-        </dl>
-      </li>
-      <li>
-        <dl>
-          <dt>順位</dt>
-          <dd>
-            <ul>
-              <li class="cat01">第１候補</li>
-              <li class="cat02">第２候補</li>
-              <li class="cat03">第３候補</li> 
-            </ul>
-          </dd>
-        </dl>
-      </li>
-  <!--/sort-btn--></ul>
+              
+              
+              <div class="flex">
+                <!--更新ボタン-->
+                <form action="{{ route('photo.edit',$photo->id) }}" method="GET" class="text-left">
+                    @csrf
+                      <button type="submit">
+                       <i class="far fa-edit"></i>
+                      </button>
+                </form>
+                <!--削除ボタン-->
+                <form action="{{ route('photo.destroy',$photo->id) }}" method="POST" class="text-right">
+                    @method('delete')
+                    @csrf
+                      <button class="destroy" type="submit">
+                       <i class="fas fa-trash"></i>
+                      </button>
+                </form>
+              </div>
+            </div>
+          </li>
+        @endforeach
+          </ul>
+          {{$photos->links()}}
+      
   
-  <ul class="grid">
-    @foreach ($photos as $photo)
-      <li class="item {{$photo->tag_no}} type_no_{{$photo->type_id}} cat03">
-        <!--内側のdivには高さを維持するためにitem-contentというクラス名をつける。-->
-        <div class="item-content">
-          <div>投稿日：{{ date('Y年m月d日', strtotime($photo->created_at)) }}</div>
-          <a href="{{ asset('storage/image/'.$photo->image)}}" data-lightbox="picture" data-title="{{$photo->name}}">
-              <img src="{{ asset('storage/image/'.$photo->image)}}" class="modal-trigger mx-auto" >
-          </a>
-          
-          <span class="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-md text-xs font-medium bg-red-100 text-blue-800">{{$photo->type_name}}</span>
-          
-          @foreach ($photo->tag_names as $tag_name)
-          <span class="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-md text-xs font-medium bg-blue-100 text-blue-800">{{$tag_name}}</span>
-          @endforeach
-          
-          
-          <div class="flex">
-            <!--更新ボタン-->
-            <form action="{{ route('photo.edit',$photo->id) }}" method="GET" class="text-left">
-                @csrf
-                  <button type="submit">
-                   <i class="far fa-edit"></i>
-                  </button>
-            </form>
-            <!--削除ボタン-->
-            <form action="{{ route('photo.destroy',$photo->id) }}" method="POST" class="text-right">
-                @method('delete')
-                @csrf
-                  <button class="destroy" type="submit">
-                   <i class="fas fa-trash"></i>
-                  </button>
-            </form>
-          </div>
-        </div>
-      </li>
-    @endforeach
-  </ul>
-  {{$photos->links()}}
-  
-  
-  <script>
-    lightbox.option({
-      'resizeDuration': 500,
-      'wrapAround': true,
-      'alwaysShowNavOnTouchDevices':true,
-    })
-</script>
-  
+            <script>
+              lightbox.option({
+                'resizeDuration': 500,
+                'wrapAround': true,
+                'alwaysShowNavOnTouchDevices':true,
+              })
+          </script>
+      </div>
+    </div>
 
   </x-app-layout> 
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/velocity/1.0.0/velocity.min.js"></script>
+<script type="text/javascript">
+(function($) {
+    $(function () {
+        $('.sub-menu > a').on('click', function (e) {
+            e.preventDefault();
+            var $subNav = $(this).next('.sub-menu-nav');
+            if ($subNav.css("display") === "none") {
+                $(this).addClass('is-active');
+                $subNav.velocity('slideDown', {duration: 400});
+            } else {
+                $(this).removeClass('is-active');
+                $subNav.velocity('slideUp', {duration: 400});
+            }
+        });
+    });
+})(jQuery);
+</script>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>   
   <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
