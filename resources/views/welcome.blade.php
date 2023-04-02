262b777c2984cdf55297dc0392d7f8234897b583
hayaharu3220 <hayato.314.nico@gmail.com>
2023-04-02 16:00:43 +0000

    アプリ名修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>My Hoem Album</title>
+        <title>家づくりアルバム</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -26,7 +26,7 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">マイページ</a>
                     @else
                     
                             <a href="{{ route('login') }}" class="ml-4 text-9xl dark:text-gray-500 underline">ログイン</a>
@@ -39,7 +39,7 @@
             @endif
             <div class="main">
                 <img class="family-picture" src="{{ asset('images//family.jpg')}}"  >
-                <h1>My Home Album</h1>
+                <h1>家づくりアルバム</h1>
             </div>
         </div>
         
