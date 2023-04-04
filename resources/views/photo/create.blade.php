b6734758c3d18411df59aa7ba7eb3416cef80671
hayaharu3220 <hayato.314.nico@gmail.com>
2023-04-04 13:17:40 +0000

    head部分の不要なコード削除

--- resources/views/photo/create.blade.php
+++ resources/views/photo/create.blade.php
@@ -1,10 +1,5 @@
 <!-- resources/views/tweet/create.blade.php -->
   <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-
-      <title>イエつく！！</title>
-    
       <!-- Select2.css -->
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/css/select2.min.css">
       <!-- jquery & iScroll -->
@@ -12,14 +7,6 @@
       <!-- Select2本体 -->
       <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/js/select2.min.js"></script>
 
-      <!-- Fonts -->
-      <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-      <link rel="stylesheet" href="{{ asset('css/radiobutton.css') }}">
-      <style>
-          body {
-              font-family: 'Nunito', sans-serif;
-          }
-      </style>
   </head>
     
   <x-app-layout>
