97dba8b39964a9bbc91db320d74e3d81a61c3236
hayaharu3220 <hayato.314.nico@gmail.com>
2023-04-05 17:09:46 +0000

    タイトル修正、タグ編集画面のバグ改修

--- resources/views/photo/edit.blade.php
+++ resources/views/photo/edit.blade.php
@@ -2,11 +2,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>イエつく！！</title>
-      
         <!-- Select2.css -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/css/select2.min.css">
         <!-- jquery & iScroll -->
@@ -14,14 +9,7 @@
         <!-- Select2本体 -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/js/select2.min.js"></script>
         <script src="{{ asset('js/test.js') }}"></script>
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
     </head>
     <body>
       <x-app-layout>
