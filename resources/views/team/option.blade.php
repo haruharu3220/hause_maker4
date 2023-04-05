0ecd58aab8cf2e5fb481f941fca6c4491bff10da
hayaharu3220 <hayato.314.nico@gmail.com>
2023-04-05 12:46:25 +0000

    チームIDを8文字以下に設定

--- resources/views/team/option.blade.php
+++ resources/views/team/option.blade.php
@@ -2,11 +2,11 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
     
-    <h1>{{$user->name}}さん。はじめまして</h1>
-    <h1>既に家族アカウントを作成していますか？</h1>
-    <p>作成している場合は家族IDを入力することで参加することができます</p>
+    <h1 class="text-2xl m-4 text-center">{{$user->name}}さん。はじめまして</h1>
+    <p  class="m-4 text-center">既に家族アカウントを作成していますか？</p>
+    <p class="m-4 text-center">家族アカウントを作成している場合は家族IDを入力することで参加することができます</p>
     <br>
-    <p>まだ、作成していていない方は新規作成をしてください。</p>
+    <p class="m-4 text-center">まだ、作成していていない方は新規作成をしてください。</p>
     <a href="join">
         <x-secondary-button class="ml-3">
         {{ __('家族アカウント作成済') }}
