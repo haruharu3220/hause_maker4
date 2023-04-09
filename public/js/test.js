19da34da6fc59e1121af747eb20a854089854b4c
hayaharu3220 <hayato.314.nico@gmail.com>
2023-04-09 16:48:17 +0000

    AND検索を部分的にOR検索、全体をAND検索に変更

--- public/js/test.js
+++ public/js/test.js
@@ -47,26 +47,36 @@ $(window).on('load', function () {
   function filterDo() {
     var selectElms = $(".sort-btn ul li.active");
     var selectElemAry = [];
+    
+    
     $.each(selectElms, function (index, selectElm) {
+      //クラス名を取得し、スペースで分割
       var className = $(this).attr("class");
       className = className.split(' ');
+      //クラス名の先頭にピリオドを追加し、selectElemAryに追加
       selectElemAry.push("." + className[0]);
     });
 
     if (selectElemAry.length === 0 || selectElemAry.includes(".all")) {
       grid.show('');
     } else {
+      var tagFilters = selectElemAry.filter(className => className.startsWith(".tag_no_"));
+      var typeFilters = selectElemAry.filter(className => className.startsWith(".type_no_"));
+
       grid.filter(function (item) {
-        return selectElemAry.every(function (className) {
+        var matchTags = tagFilters.length === 0 || tagFilters.some(function (className) {
           return item.getElement().classList.contains(className.substring(1));
         });
+
+        var matchTypes = typeFilters.length === 0 || typeFilters.some(function (className) {
+          return item.getElement().classList.contains(className.substring(1));
+        });
+
+        return matchTags && matchTypes;
       });
     }
   }
-  
-
 });
-
 
 function toggleChevron(e) {
     $(e.target)
