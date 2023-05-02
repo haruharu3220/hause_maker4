175b7b0a74afe672e986b92fa9ce6ff2ead6055a
hayaharu3220 <hayato.314.nico@gmail.com>
2023-05-02 14:16:24 +0000

    featureブランチを一時待避

---
+++ database/migrations/2023_04_26_221156_add_shared_at_to_photos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('photos', function (Blueprint $table) {
+            //
+            $table->timestamp('shared_at')->nullable()->after('share_flag');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('photos', function (Blueprint $table) {
+            //
+            $table->dropColumn('shared_at');
+        });
+    }
+};
